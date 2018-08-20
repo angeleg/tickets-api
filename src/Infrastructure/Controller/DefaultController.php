9a133d5b77134354e6a2ec3118a4d5ac96c08180
angele <angele@usabilla.com>
2018-08-21 00:03:32 +0200

    Install Symfony 4 and various packages
    Packages installed:
    - JMS Serializer
    - Tactician (Command Bus)
    - Prophecy (mocking framework)
    - PHPStan (static analysis tool)
    - PHP Code Sniffer / Beautifier
    - UUID
    - Money

---
+++ src/Infrastructure/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Infrastructure\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index()
+    {
+        return $this->render('base.html.twig', []);
+    }
+}
